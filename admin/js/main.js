4e7603201312d0289ae6a3e3a085066a531a34f2
Fabian Frei <fabian.frei@esf-frei.ch>
2013-03-06 01:01:20 +0100

    added skeletalcode with simple server script and a simple jquery plugin

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,1 +1,3 @@
-
+$(document).ready(function($) {
+	$.lessCode.backEndConnect("body");
+});
--- admin/js/plugins.js
+++ admin/js/plugins.js
@@ -22,3 +22,61 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+(function($){
+    if(!$.lessCode){
+        $.lessCode = new Object();
+    };
+    
+    $.lessCode.backEndConnect = function(el, options){
+        // To avoid scope issues, use 'base' instead of 'this'
+        // to reference this class from internal events and functions.
+        var base = this;
+        
+        // Access to jQuery and DOM versions of element
+        base.$el = $(el);
+        base.el = el;
+        
+        // Add a reverse reference to the DOM object
+        base.$el.data("lessCode.backEndConnect", base);
+        
+        base.init = function(){
+            base.options = $.extend({},$.lessCode.backEndConnect.defaultOptions, options);
+
+            // add eventlistener for all contenteditable
+            base.$el.find("*[contenteditable]").bind("input", function(event) {
+                var data = {
+                    id : $(this).attr("id"),
+                    content : $(this).html()
+                };
+                $.post('http://localhost:8080/', data, function(data) {
+                    console.log(data);
+                });
+                console.log(data);
+            });            
+        };
+        
+        // Sample Function, Uncomment to use
+        // base.functionName = function(paramaters){
+        // 
+        // };
+        
+        // Run initializer
+        base.init();
+    };
+    
+    $.lessCode.backEndConnect.defaultOptions = {
+    };
+    
+    $.fn.lesscode_backEndConnect = function(element, options){
+        return this.each(function(){
+            (new $.lessCode.backEndConnect(this, element, options));
+        });
+    };
+    
+    // This function breaks the chain, but returns
+    // the lessCode.backEndConnect if it has been attached to the object.
+    $.fn.getlessCode_backEndConnect = function(){
+        this.data("lessCode.backEndConnect");
+    };
+    
+})(jQuery);
